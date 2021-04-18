db7e8e3ae670072f2ecdb31056ed3d061de1ae08
Alessandro Vinci <alessandrovinciabc@gmail.com>
2021-04-18 13:16:35 +0200

    Style preview

--- src/Components/Preview/Preview.js
+++ src/Components/Preview/Preview.js
@@ -4,9 +4,29 @@ import './Preview.css';
 
 function Preview(props) {
   return (
-    <div className="Preview">
-      <img src={props.currentImg} alt="" />
-      <div>{props.skills}</div>
+    <div className="PreviewContainer">
+      <div className="Preview">
+        <div className="SideBar">
+          {props.currentImg && (
+            <img
+              className="Preview__image"
+              src={props.currentImg}
+              alt=""
+              draggable="false"
+            />
+          )}
+          <div className="Skill-list">
+            {props.skills.map((skill) => {
+              return (
+                <div className="Skill" key={skill.id}>
+                  {skill.text}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="Main"></div>
+      </div>
     </div>
   );
 }
