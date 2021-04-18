9159a34a1db67b5850bbd8171d09887517e9669f
Alessandro Vinci <alessandrovinciabc@gmail.com>
2021-04-18 09:35:54 +0200

    Lift state up to the App component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,12 +5,160 @@ import './App.css';
 import Header from '../Header/Header.js';
 import Builder from '../Builder/Builder.js';
 
+import { v4 as uuidv4 } from 'uuid';
+const NUMBER_OF_INPUTS = 4;
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const sections = ['Education', 'Experience', 'Projects'];
+
+    let createSection = (title) => {
+      return {
+        id: uuidv4(),
+        title: title,
+        input: Array(NUMBER_OF_INPUTS).fill(''),
+        list: [],
+      };
+    };
+
+    this.state = {
+      previewMode: false,
+      currentImg: '',
+      skillInput: '',
+      skills: [],
+      sections: sections.map(createSection),
+    };
+
+    this.handleImgChange = this.handleImgChange.bind(this);
+    this.handleSkillInputChange = this.handleSkillInputChange.bind(this);
+    this.handleSkillAdd = this.handleSkillAdd.bind(this);
+    this.handleSkillDelete = this.handleSkillDelete.bind(this);
+    this.handleExperienceInputs = this.handleExperienceInputs.bind(this);
+    this.handleExperienceAdd = this.handleExperienceAdd.bind(this);
+    this.handleExperienceDelete = this.handleExperienceDelete.bind(this);
+    this.handleExperienceFormReset = this.handleExperienceFormReset.bind(this);
+  }
+
+  handleImgChange(e) {
+    let newFileURL = URL.createObjectURL(e.target.files[0]);
+
+    this.setState((state) => {
+      if (state.currentImg) {
+        URL.revokeObjectURL(state.currentImg);
+      }
+      return { currentImg: newFileURL };
+    });
+  }
+
+  handleSkillInputChange(e) {
+    this.setState({ skillInput: e.target.value });
+  }
+
+  handleSkillAdd(e) {
+    this.setState((state) => {
+      if (state.skillInput !== '') {
+        return {
+          skills: state.skills.concat([
+            { id: uuidv4(), text: state.skillInput },
+          ]),
+          skillInput: '',
+        };
+      }
+    });
+  }
+
+  handleSkillDelete(index) {
+    this.setState((state) => {
+      let filtered = state.skills.slice();
+      filtered.splice(index, 1);
+
+      return {
+        skills: filtered,
+      };
+    });
+  }
+
+  handleExperienceInputs(e, sectionNumber) {
+    let indexOfInput = e.target.dataset.index;
+    this.setState((state) => {
+      let copy = JSON.parse(JSON.stringify(state.sections));
+      copy[sectionNumber].input[indexOfInput] = e.target.value;
+      return { sections: copy };
+    });
+  }
+
+  handleExperienceAdd(sectionNumber) {
+    this.setState((state) => {
+      let copy = JSON.parse(JSON.stringify(state.sections));
+
+      copy[sectionNumber].list.push({
+        id: uuidv4(),
+        title: state.sections[sectionNumber].input[0],
+        description: state.sections[sectionNumber].input[1],
+        startDate: state.sections[sectionNumber].input[2],
+        endDate: state.sections[sectionNumber].input[3],
+      });
+
+      copy[sectionNumber].input = Array(NUMBER_OF_INPUTS).fill('');
+
+      return {
+        sections: copy,
+      };
+    });
+  }
+
+  handleExperienceDelete(e, sectionNumber) {
+    let index = e.target.dataset.index;
+    this.setState((state) => {
+      let copy = JSON.parse(JSON.stringify(state.sections));
+      copy[sectionNumber].list.splice(index, 1);
+      return { sections: copy };
+    });
+  }
+
+  handleExperienceFormReset(sectionNumber) {
+    this.setState((state) => {
+      let copy = JSON.parse(JSON.stringify(state.sections));
+      copy[sectionNumber].input = Array(NUMBER_OF_INPUTS).fill('');
+
+      return { sections: copy };
+    });
+  }
+
   render() {
+    let skillHandlers, sectionHandlers;
+
+    skillHandlers = {
+      input: this.handleSkillInputChange,
+      add: this.handleSkillAdd,
+      delete: this.handleSkillDelete,
+    };
+
+    sectionHandlers = {
+      input: this.handleExperienceInputs,
+      reset: this.handleExperienceFormReset,
+      add: this.handleExperienceAdd,
+      delete: this.handleExperienceDelete,
+    };
+
+    let handlers = {
+      img: this.handleImgChange,
+      skill: skillHandlers,
+      section: sectionHandlers,
+    };
+
     return (
       <div className="App">
         <Header logo="CV BUILDER" />
-        <Builder />
+        <Builder
+          handlers={handlers}
+          currentImg={this.state.currentImg}
+          skillInput={this.state.skillInput}
+          skills={this.state.skills}
+          sections={this.state.sections}
+        />
       </div>
     );
   }
--- src/Components/Builder/Builder.js
+++ src/Components/Builder/Builder.js
@@ -11,146 +11,9 @@ import InfoInput from '../InfoInput/InfoInput.js';
 import SkillList from '../SkillList/SkillList.js';
 import ExperienceList from '../ExperienceList/ExperienceList.js';
 
-import { v4 as uuidv4 } from 'uuid';
-const NUMBER_OF_INPUTS = 4;
-
 class Builder extends React.Component {
-  constructor(props) {
-    super(props);
-
-    const sections = ['Education', 'Experience', 'Projects'];
-
-    let createSection = (title) => {
-      return {
-        id: uuidv4(),
-        title: title,
-        input: Array(NUMBER_OF_INPUTS).fill(''),
-        list: [],
-      };
-    };
-
-    this.state = {
-      currentImg: '',
-      skillInput: '',
-      skills: [],
-      sections: sections.map(createSection),
-    };
-
-    this.handleImgChange = this.handleImgChange.bind(this);
-    this.handleSkillInputChange = this.handleSkillInputChange.bind(this);
-    this.handleSkillAdd = this.handleSkillAdd.bind(this);
-    this.handleSkillDelete = this.handleSkillDelete.bind(this);
-    this.handleExperienceInputs = this.handleExperienceInputs.bind(this);
-    this.handleExperienceAdd = this.handleExperienceAdd.bind(this);
-    this.handleExperienceDelete = this.handleExperienceDelete.bind(this);
-    this.handleExperienceFormReset = this.handleExperienceFormReset.bind(this);
-  }
-
-  handleImgChange(e) {
-    let newFileURL = URL.createObjectURL(e.target.files[0]);
-
-    this.setState((state) => {
-      if (state.currentImg) {
-        URL.revokeObjectURL(state.currentImg);
-      }
-      return { currentImg: newFileURL };
-    });
-  }
-
-  handleSkillInputChange(e) {
-    this.setState({ skillInput: e.target.value });
-  }
-
-  handleSkillAdd(e) {
-    this.setState((state) => {
-      if (state.skillInput !== '') {
-        return {
-          skills: state.skills.concat([
-            { id: uuidv4(), text: state.skillInput },
-          ]),
-          skillInput: '',
-        };
-      }
-    });
-  }
-
-  handleSkillDelete(index) {
-    this.setState((state) => {
-      let filtered = state.skills.slice();
-      filtered.splice(index, 1);
-
-      return {
-        skills: filtered,
-      };
-    });
-  }
-
-  handleExperienceInputs(e, sectionNumber) {
-    let indexOfInput = e.target.dataset.index;
-    this.setState((state) => {
-      let copy = JSON.parse(JSON.stringify(state.sections));
-      copy[sectionNumber].input[indexOfInput] = e.target.value;
-      return { sections: copy };
-    });
-  }
-
-  handleExperienceAdd(sectionNumber) {
-    this.setState((state) => {
-      let copy = JSON.parse(JSON.stringify(state.sections));
-
-      copy[sectionNumber].list.push({
-        id: uuidv4(),
-        title: state.sections[sectionNumber].input[0],
-        description: state.sections[sectionNumber].input[1],
-        startDate: state.sections[sectionNumber].input[2],
-        endDate: state.sections[sectionNumber].input[3],
-      });
-
-      copy[sectionNumber].input = Array(NUMBER_OF_INPUTS).fill('');
-
-      return {
-        sections: copy,
-      };
-    });
-  }
-
-  handleExperienceDelete(e, sectionNumber) {
-    let index = e.target.dataset.index;
-    this.setState((state) => {
-      let copy = JSON.parse(JSON.stringify(state.sections));
-      copy[sectionNumber].list.splice(index, 1);
-      return { sections: copy };
-    });
-  }
-
-  handleExperienceFormReset(sectionNumber) {
-    this.setState((state) => {
-      let copy = JSON.parse(JSON.stringify(state.sections));
-      copy[sectionNumber].input = Array(NUMBER_OF_INPUTS).fill('');
-
-      return { sections: copy };
-    });
-  }
-
   render() {
-    let { currentImg, skillInput, skills } = this.state;
-
-    let {
-      handleImgChange,
-      handleSkillInputChange,
-      handleSkillAdd,
-      handleSkillDelete,
-      handleExperienceInputs,
-      handleExperienceAdd,
-      handleExperienceDelete,
-      handleExperienceFormReset,
-    } = this;
-
-    let skillHandlers = {
-      input: handleSkillInputChange,
-      add: handleSkillAdd,
-      delete: handleSkillDelete,
-    };
+    let { handlers, currentImg, skillInput, skills, sections } = this.props;
 
     return (
       <div className="Builder">
@@ -159,30 +22,30 @@ class Builder extends React.Component {
             className="Builder__card"
             currentImg={currentImg}
             defaultImg={defaultImg}
-            onChange={handleImgChange}
+            onChange={handlers.img}
           />
           <InfoInput />
         </div>
-        <SkillList input={skillInput} list={skills} handlers={skillHandlers} />
+        <SkillList input={skillInput} list={skills} handlers={handlers.skill} />
 
         {/*Education, Experience, Projects sections*/}
-        {this.state.sections.map((section, index) => {
+        {sections.map((section, index) => {
           return (
             <ExperienceList
               key={section.id}
               name={section.title}
               inputs={section.input}
               handler={(e) => {
-                handleExperienceInputs(e, index);
+                handlers.section.input(e, index);
               }}
               onAdd={() => {
-                handleExperienceAdd(index);
+                handlers.section.add(index);
               }}
               onDelete={(e) => {
-                handleExperienceDelete(e, index);
+                handlers.section.delete(e, index);
               }}
               onReset={() => {
-                handleExperienceFormReset(index);
+                handlers.section.reset(index);
               }}
               list={section.list}
             />
