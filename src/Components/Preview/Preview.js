475753e03396c1534773e063acbabaede7f57096
Alessandro Vinci <alessandrovinciabc@gmail.com>
2021-04-18 14:36:47 +0200

    Install react-pdf and use it for the preview component

--- src/Components/Preview/Preview.js
+++ src/Components/Preview/Preview.js
@@ -1,32 +1,67 @@
 import React from 'react';
+import {
+  Page,
+  Image,
+  View,
+  Document,
+  StyleSheet,
+  PDFViewer,
+} from '@react-pdf/renderer';
 
 import './Preview.css';
+
+// Create styles
+const styles = StyleSheet.create({
+  pictureContainer: {
+    border: '2 solid white',
+    borderRadius: '100%',
+
+    width: 100,
+    height: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    marginTop: 20,
+  },
+  image: {
+    width: 90,
+    height: 90,
+    objectFit: 'cover',
+    objectPosition: 'center',
+
+    borderRadius: '100%',
+  },
+  page: {
+    flexDirection: 'row',
+  },
+  section: {
+    backgroundColor: 'rgb(66, 152, 223)',
+    height: '100%',
+    width: '30%',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+});
+
+// Create Document Component
+const MyDocument = (props) => (
+  <Document>
+    <Page size="A4" style={styles.page}>
+      <View style={styles.section}>
+        <View style={styles.pictureContainer}>
+          <Image src={props.img} style={styles.image} />
+        </View>
+      </View>
+    </Page>
+  </Document>
+);
 
 function Preview(props) {
   return (
     <div className="PreviewContainer">
-      <div className="Preview">
-        <div className="SideBar">
-          {props.currentImg && (
-            <img
-              className="Preview__image"
-              src={props.currentImg}
-              alt=""
-              draggable="false"
-            />
-          )}
-          <div className="Skill-list">
-            {props.skills.map((skill) => {
-              return (
-                <div className="Skill" key={skill.id}>
-                  {skill.text}
-                </div>
-              );
-            })}
-          </div>
-        </div>
-        <div className="Main"></div>
-      </div>
+      <PDFViewer width="595px" height="842px">
+        <MyDocument img={props.currentImg}></MyDocument>
+      </PDFViewer>
     </div>
   );
 }
