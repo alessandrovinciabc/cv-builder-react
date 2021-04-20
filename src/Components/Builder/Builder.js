328894de55d74f47965372a385940a4f83c10c82
Alessandro Vinci <alessandrovinciabc@gmail.com>
2021-04-20 18:29:25 +0200

    Convert Builder into functional component

--- src/Components/Builder/Builder.js
+++ src/Components/Builder/Builder.js
@@ -11,52 +11,47 @@ import InfoInput from '../InfoInput/InfoInput.js';
 import SkillList from '../SkillList/SkillList.js';
 import ExperienceList from '../ExperienceList/ExperienceList.js';
 
-class Builder extends React.Component {
-  render() {
-    let { handlers, currentImg, skillInput, skills, sections } = this.props;
+function Builder(props) {
+  let { handlers, currentImg, skillInput, skills, sections } = props;
 
-    return (
-      <div className="Builder">
-        <div className="Builder__info">
-          <ImagePicker
-            className="Builder__card"
-            currentImg={currentImg}
-            defaultImg={defaultImg}
-            onChange={handlers.img}
-          />
-          <InfoInput
-            info={this.props.info}
-            handler={this.props.handleInfoChange}
-          />
-        </div>
-        <SkillList input={skillInput} list={skills} handlers={handlers.skill} />
-
-        {/*Education, Experience, Projects sections*/}
-        {sections.map((section, index) => {
-          return (
-            <ExperienceList
-              key={section.id}
-              name={section.title}
-              inputs={section.input}
-              handler={(e) => {
-                handlers.section.input(e, index);
-              }}
-              onAdd={() => {
-                handlers.section.add(index);
-              }}
-              onDelete={(e) => {
-                handlers.section.delete(e, index);
-              }}
-              onReset={() => {
-                handlers.section.reset(index);
-              }}
-              list={section.list}
-            />
-          );
-        })}
+  return (
+    <div className="Builder">
+      <div className="Builder__info">
+        <ImagePicker
+          className="Builder__card"
+          currentImg={currentImg}
+          defaultImg={defaultImg}
+          onChange={handlers.img}
+        />
+        <InfoInput info={props.info} handler={props.handleInfoChange} />
       </div>
-    );
-  }
+      <SkillList input={skillInput} list={skills} handlers={handlers.skill} />
+
+      {/*Education, Experience, Projects sections*/}
+      {sections.map((section, index) => {
+        return (
+          <ExperienceList
+            key={section.id}
+            name={section.title}
+            inputs={section.input}
+            handler={(e) => {
+              handlers.section.input(e, index);
+            }}
+            onAdd={() => {
+              handlers.section.add(index);
+            }}
+            onDelete={(e) => {
+              handlers.section.delete(e, index);
+            }}
+            onReset={() => {
+              handlers.section.reset(index);
+            }}
+            list={section.list}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export default Builder;
